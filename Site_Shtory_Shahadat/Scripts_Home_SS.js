26f3fc6e92f06de13fe04e2c323f0cb2abe88e4d
Nefe0l <115877374+Nefe0l@users.noreply.github.com>
2022-12-12 01:33:09 +0600

    Add files via upload

---
+++ Site_Shtory_Shahadat/Scripts_Home_SS.js
@@ -0,0 +1,61 @@
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("activator", window.scrollY > 0);
+  });
+  window.addEventListener("scroll", function () {
+    var Effect = document.querySelector(".Effect");
+    Effect.classList.toggle("active", window.scrollY > 0);
+  });
+  window.addEventListener("scroll", function () {
+    var Color = document.querySelector(".Color");
+    Color.classList.toggle("active", window.scrollY > 0);
+  });
+
+
+  let navigation = document.querySelector(".Ion-cons_Shtory_Shahadat");
+  let navigation_2 = document.querySelector(".Section_Text_Div_Shtory_Shahadat");
+  let navigation_3 = document.querySelector(".Div_Effect_2_Shtory_Shahadat");
+  let navigation_4 = document.querySelector(".Effect_Img_2_Shtory_Shahadat");
+  let navigation_5 = document.querySelector(".Effect_Dark_Img_Shtory_Shahadat");
+  let navigation_6 = document.querySelector(".Div_Effect_2_A_Shtory_Shahadat");
+  navigation.onclick = function () {
+    navigation.classList.toggle("active");
+    navigation_2.classList.toggle("active_2");
+    navigation_3.classList.toggle("active_3");
+    navigation_4.classList.toggle("active_4");
+    navigation_5.classList.toggle("active_5");
+    navigation_6.classList.toggle("active_6");
+  };
+
+  let For_Burger_Button = document.querySelector(".Need_3");
+  let For_Burger_Menu = document.querySelector(".Burger_Menu");
+  let For_Burger_Menu_A = document.querySelector("nav");
+ 
+  For_Burger_Button.onclick = function () {
+  For_Burger_Button.classList.toggle("active_B")
+  For_Burger_Menu.classList.toggle("active_BM")
+  For_Burger_Menu_A.classList.toggle('active_A')
+}
+
+
+  let Close = document.querySelector('.Nav_ion_cons_close');
+  let Close_2 = document.querySelector('.Grey_a');
+  let Close_3 = document.querySelector('.Test_a');
+  let Close_4 = document.querySelector('.Test_a_2');
+  let Close_5 = document.querySelector('.Test_a_3');
+  Close.onclick = function () {
+    Close.classList.toggle("Close_function");
+    Close_2.classList.toggle("Close_function_2");
+    Close_3.classList.toggle("Close_function_3");
+    Close_4.classList.toggle("Close_function_4");
+    Close_5.classList.toggle("Close_function_5");
+  }
+
+  const Color = document.querySelector(".Color");
+  const header = document.querySelector("header");
+
+  Color.onclick = function () {
+    Color.classList.toggle("active_Color_Background");
+    header.classList.toggle("active_header_Background");
+
+  };
